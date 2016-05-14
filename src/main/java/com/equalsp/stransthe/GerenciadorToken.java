2253601fc50f39fd82a73708294a7ec59c08e747
Luan Pontes <luanpontes2@gmail.com>
2016-05-14 13:38:06 -0300

    Disponibilizar projeto

---
+++ src/main/java/com/equalsp/stransthe/GerenciadorToken.java
@@ -0,0 +1,75 @@
+package com.equalsp.stransthe;
+
+import static com.equalsp.stransthe.Utils.dateFormated;
+import static com.equalsp.stransthe.Utils.inputStreamToString;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.util.Date;
+import java.util.Map;
+
+import static com.equalsp.stransthe.Utils.*;
+
+public class GerenciadorToken {
+	
+	public static final String URL_SERVICE = "https://api.inthegra.strans.teresina.pi.gov.br/v1/";
+
+	private String email;
+	private String senha;
+	private String key;
+	
+	private String token;
+	
+	
+	public GerenciadorToken(String email, String senha, String key) {
+		this.email = email;
+		this.senha = senha;
+		this.key = key;
+	}
+
+	public String autenticar() throws Exception{
+		String requestJson = "{\"email\": \""+email+"\",\"password\": \""+senha+"\"}";
+		
+		HttpURLConnection connection = criarConnection("signin");
+		setBody(requestJson, connection);
+		connection.connect();
+		
+		int responseCode = connection.getResponseCode();
+		if(responseCode == 404){
+			throw new IllegalArgumentException("A url "+_url+" é inválida. (Erro 404)");
+		}else if(responseCode == 500){
+			throw new RuntimeException("Erro interno do servidor da API. (Erro 500)");
+		}
+		String responseJson = inputStreamToString(connection.getInputStream());
+		connection.disconnect();
+		return responseJson;
+	}
+
+	private HttpURLConnection criarConnection(String _url)
+			throws MalformedURLException, IOException, ProtocolException {
+		
+		URL url = new URL(URL_SERVICE+_url);
+		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+		connection.setRequestMethod("POST");
+		connection.setDoOutput(true);
+		connection.setUseCaches(false);
+		connection.setConnectTimeout(15000);
+		connection.setRequestProperty("Content-Type", "application/json");
+		connection.setRequestProperty("Accept-Language", "en");
+		connection.setRequestProperty("Date", dateFormated(new Date()));
+		connection.setRequestProperty("X-Api-Key","49ea6f5525a34e71bdd7b4f8a92adaac");
+		return connection;
+	}
+	
+	public static void main(String[] args) throws Exception {
+		GerenciadorToken g = new GerenciadorToken("luanpontes2@gmail.com", "naul1991", "49ea6f5525a34e71bdd7b4f8a92adaac");
+		System.out.println(g.autenticar());
+	}
+	
+	
+}
---
+++ src/main/java/com/equalsp/stransthe/Utils.java
@@ -0,0 +1,76 @@
+package com.equalsp.stransthe;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.io.UnsupportedEncodingException;
+import java.io.Writer;
+import java.net.HttpURLConnection;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ * Alguns métodos utilitários. 
+ * 
+ * @author toolmaker
+ *
+ */
+public class Utils {
+	
+	private static final String DATA_FORMAT = "E, dd MMM yyyy HH:mm:ss 'GMT'";
+
+	/**
+	 * Retorna retorna um String a partir de um InputStream. 
+	 * 
+	 * @param is
+	 * @return
+	 * @throws IOException
+	 */
+	public static String inputStreamToString(InputStream is) throws IOException {
+		if (is != null) {
+			Writer writer = new StringWriter();
+
+			char[] buffer = new char[1024];
+			try {
+				Reader reader = new BufferedReader(new InputStreamReader(is,"UTF-8"));
+				int n;
+				while ((n = reader.read(buffer)) != -1) {
+					writer.write(buffer, 0, n);
+				}
+			} finally {
+				is.close();
+			}
+			return writer.toString();
+		} else {
+			return "";
+		}
+	}
+	
+	public static String dateFormated(Date data){
+		SimpleDateFormat formatter = new SimpleDateFormat(DATA_FORMAT,Locale.US);
+		return formatter.format(data);
+	}
+	
+	
+	/**
+	 * Seta o conteúdo (body) passado por parametro
+	 * na conexão também passada. 
+	 * 
+	 * @param body
+	 * @param connection
+	 * @throws IOException
+	 * @throws UnsupportedEncodingException
+	 */
+	public static void setBody(String body, HttpURLConnection connection) 
+				throws IOException, UnsupportedEncodingException {
+		DataOutputStream stream = new DataOutputStream(connection.getOutputStream());
+		stream.write(body.getBytes("UTF-8"));
+		stream.flush();
+		stream.close();
+	}
+}
