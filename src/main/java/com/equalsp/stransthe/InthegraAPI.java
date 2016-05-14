3584f48e3450c908b1906dd1a258a16122f6acfd
erickpassos <erickpassos@gmail.com>
2016-05-14 19:17:43 -0300

    Comentarios APU

--- src/main/java/com/equalsp/stransthe/InthegraAPI.java
+++ src/main/java/com/equalsp/stransthe/InthegraAPI.java
@@ -3,15 +3,34 @@ package com.equalsp.stransthe;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Oferece acesso à API REST Inthegra do sistema de transporte coletivo da cidade de Teresina-PI.
+ * Registro na API em https://inthegra.strans.teresina.pi.gov.br
+ * @author Erick Passos
+ *
+ */
 public class InthegraAPI {
 
 	private static InthegraAgent tokenManager;
 
+	/**
+	 * [obrigatório] Inicializa a API antes de se fazerem buscas. 
+	 * @param email - cadastrado ao se fazer o registro na API.
+	 * @param senha - cadastrada ao se fazer o registro na API.
+	 * @param key - chave de aplicação (cadastrada através site da API). 
+	 * @return true - caso a autenticação funcione.
+	 * @throws Exception - caso ocorra um erro (email, senha, ou problema técnico).
+	 */
 	public static boolean init(String email, String senha, String key) throws Exception {
 		tokenManager = new InthegraAgent(email, senha, key);
 		return tokenManager.autenticar();
 	}
 
+	/**
+	 * Busca dados de todas as linhas cadastradas. 
+	 * @return lista com todas as linhas.
+	 * @throws Exception - caso não inicializado corretamente.
+	 */
 	public static List<Linha> getLinhas() throws Exception {
 		if (tokenManager == null) {
 			throw new Exception("Chame init(email, senha, key) antes...");
@@ -19,6 +38,11 @@ public class InthegraAPI {
 		return tokenManager.getLinhas();
 	}
 
+	/**
+	 * Busca dados de linhas com base na descrição enviada. 
+	 * @return lista com as linhas que satisfazem o critério de busca.
+	 * @throws Exception - caso não inicializado corretamente.
+	 */
 	public static List<Linha> getLinhas(String busca) throws Exception {
 		if (tokenManager == null) {
 			throw new Exception("Chame init(email, senha, key) antes...");
@@ -26,6 +50,11 @@ public class InthegraAPI {
 		return tokenManager.getLinhas(busca);
 	}
 
+	/**
+	 * Busca dados de todas as paradas cadastradas. 
+	 * @return lista com todas as paradas.
+	 * @throws Exception - caso não inicializado corretamente.
+	 */
 	public static List<Parada> getParadas() throws Exception {
 		if (tokenManager == null) {
 			throw new Exception("Chame init(email, senha, key) antes...");
@@ -33,6 +62,11 @@ public class InthegraAPI {
 		return tokenManager.getParadas();
 	}
 
+	/**
+	 * Busca dados de paradas com base na descrição enviada. 
+	 * @return lista com as paradas que satisfazem o critério de busca.
+	 * @throws Exception - caso não inicializado corretamente.
+	 */
 	public static List<Parada> getParadas(String busca) throws Exception {
 		if (tokenManager == null) {
 			throw new Exception("Chame init(email, senha, key) antes...");
@@ -40,6 +74,11 @@ public class InthegraAPI {
 		return tokenManager.getParadas(busca);
 	}
 
+	/**
+	 * Busca dados das paradas de uma linha específica. 
+	 * @return lista com todas as paradas de uma linha.
+	 * @throws Exception - caso não inicializado corretamente.
+	 */
 	public static List<Parada> getParadas(Linha linha) throws Exception {
 		if (tokenManager == null) {
 			throw new Exception("Chame init(email, senha, key) antes...");
@@ -48,6 +87,11 @@ public class InthegraAPI {
 		return p.getParadas();
 	}
 
+	/**
+	 * Busca dados atualizados de todos os veículos cadastrados. 
+	 * @return lista com todos os veículos.
+	 * @throws Exception - caso não inicializado corretamente.
+	 */
 	public static List<Veiculo> getVeiculos() throws Exception {
 		if (tokenManager == null) {
 			throw new Exception("Chame init(email, senha, key) antes...");
@@ -60,19 +104,16 @@ public class InthegraAPI {
 		return v;
 	}
 
+	/**
+	 * Busca dados atualizados dos veículos de uma linha específica. 
+	 * @return lista com todos os veículos de uma linha.
+	 * @throws Exception - caso não inicializado corretamente.
+	 */
 	public static List<Veiculo> getVeiculos(Linha linha) throws Exception {
 		if (tokenManager == null) {
 			throw new Exception("Chame init(email, senha, key) antes...");
 		}
 		return tokenManager.getVeiculosLinha(linha.getCodigoLinha()).getLinha().getVeiculosFromJson();
-	}
-
-	public static void main(String[] args) throws Exception {
-		InthegraAPI.init("erickpassos@gmail.com", "circ51sp", "ef5f05bdedd34cada40187761d5daaa7");
-		Linha l = InthegraAPI.getLinhas("ininga").get(0);
-		System.out.println(l.getCodigoLinha());
-		Veiculo v = l.getVeiculos().get(0);
-		System.out.println(v.getCodigoVeiculo());
 	}
 
 }
