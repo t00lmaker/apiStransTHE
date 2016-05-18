5919f0470d3ed9132be8b2cd36d21c6fd7dd0094
Hugo de Oliveira Cordeiro <hugo.ocordeiro@gmail.com>
2016-05-18 01:43:25 -0300

    Serializable

--- src/main/java/com/equalsp/stransthe/rotas/PontoDeInteresse.java
+++ src/main/java/com/equalsp/stransthe/rotas/PontoDeInteresse.java
@@ -4,6 +4,8 @@ import com.equalsp.stransthe.Localizacao;
 
 public class PontoDeInteresse extends Localizacao {
 	
+	private static final long serialVersionUID = -8413819847411749211L;
+
 	public PontoDeInteresse(double lat, double l) {
 		setLat(lat);
 		setLong(l);
